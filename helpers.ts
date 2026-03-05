c3f389ccd05d0df352602bc89e4bc2f459f3f00b
mertkanat2004-byte <mertkanat2004@gmail.com>
2026-03-05 21:42:07 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:42:07.828Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T18:42:08.931Z
+Commit: Fix broken links in documentation
