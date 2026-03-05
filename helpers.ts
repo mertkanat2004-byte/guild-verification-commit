eda91ddfcadc8f79b6bec764c870d5b04805184d
mertkanat2004-byte <mertkanat2004@gmail.com>
2026-03-05 21:40:32 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:40:15.629Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T18:40:34.309Z
+Commit: Fix CSS styling bug on mobile devices
