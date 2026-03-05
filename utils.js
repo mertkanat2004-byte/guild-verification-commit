225403b6eefcc74e597abff8a5737e931bdc9268
mertkanat2004-byte <mertkanat2004@gmail.com>
2026-03-05 21:41:50 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:41:42.315Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T18:41:51.481Z
+Commit: Fix CSS styling bug on mobile devices
