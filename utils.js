d588db598010787b8d5e6c0b2e414862686fe4cd
mertkanat2004-byte <mertkanat2004@gmail.com>
2026-03-05 21:43:18 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:43:13.542Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T18:43:19.903Z
+Commit: Fix broken links in documentation
