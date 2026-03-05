115c8e01ce9a6d76629254b707eed5319f01c09a
mertkanat2004-byte <mertkanat2004@gmail.com>
2026-03-05 21:43:29 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:42:59.282Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T18:43:31.049Z
+Commit: Fix broken links in documentation
