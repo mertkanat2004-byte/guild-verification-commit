fce9c64308bef8531cfc0e79ea42bc1b4026c551
mertkanat2004-byte <mertkanat2004@gmail.com>
2026-03-05 21:44:12 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:44:02.279Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T18:44:13.966Z
+Commit: Resolve null pointer exception in user service
