f313b607a598f6b74d086b4cb6a8b26097f9dd7c
mertkanat2004-byte <mertkanat2004@gmail.com>
2026-03-05 21:42:13 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:41:52.692Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T18:42:14.600Z
+Commit: Add export functionality for reports
